f96d6d78912ea7ed35fd502a06ce46ff3e10b3cd
Sava Bežanović <Sava Beanovi>
2017-04-25 20:16:43 +0200

    'Finished'

--- app/views/admin/adminLogin.view.php
+++ app/views/admin/adminLogin.view.php
@@ -3,17 +3,13 @@
 
     <div class="col-md-2">
         <nav class="menu-left">
-
             <ul class="nav nav-pills nav-stacked">
-
-                <li class="active"><a href="/#/">Home</a></li>
-                <li class="active"><a href="/#/staff">Staff</a></li>
-                <li class="active"><a href="/#/products">Products</a></li>
-                <li class="active"><a href="/#/categories">Category</a></li>
-                <li class="active"><a href="/admin/login">Admin Login</a></li>
-
+                <li class="active"><a href="/#/"><span class="glyphicon glyphicon-home"></span> HOME</a></li>
+                <li class="active"><a href="/#/staff"><span class="glyphicon glyphicon-user"></span> STAFF</a></li>
+                <li class="active"><a href="/#/products"><span class="glyphicon glyphicon-apple"></span> PRODUCTS</a></li>
+                <li class="active"><a href="/#/categories"><span class="glyphicon glyphicon-th-list"></span> CATEGORY</a></li>
+                <li class="active"><a href="/admin/login"><span class="glyphicon glyphicon-book"></span> ADMIN LOGIN</a></li>
             </ul>
-
         </nav>
     </div>
 
--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,11 @@
          <div class="col-md-2">
             <nav class="menu-left">
                         <ul class="nav nav-pills nav-stacked">
-                            <li class="active"><a href="/#/">HOME</a></li>
-                            <li class="active"><a href="/#/staff">STAFF</a></li>
-                            <li class="active"><a href="/#/products">PRODUCTS</a></li>
-                            <li class="active"><a href="/#/categories">CATEGORY</a></li>
-                            <li class="active"><a href="/admin/login">ADMIN LOGIN</a></li>
+                            <li class="active"><a href="/#/"><span class="glyphicon glyphicon-home"></span> HOME</a></li>
+                            <li class="active"><a href="/#/staff"><span class="glyphicon glyphicon-user"></span> STAFF</a></li>
+                            <li class="active"><a href="/#/products"><span class="glyphicon glyphicon-apple"></span> PRODUCTS</a></li>
+                            <li class="active"><a href="/#/categories"><span class="glyphicon glyphicon-th-list"></span> CATEGORY</a></li>
+                            <li class="active"><a href="/admin/login"><span class="glyphicon glyphicon-book"></span> ADMIN LOGIN</a></li>
                         </ul>
             </nav>
     </div>
